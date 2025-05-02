d29aef9b04f27524ba1be0d547bb99ab8ca4f45f
HASSAN <151738906+hassanalaahassan@users.noreply.github.com>
2025-05-03 01:20:21 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 const menu = document.querySelector('#menu-icon')
 const navList = document.querySelector('.nav-list')
-console.log(menu,navList);
+const heroText = document.querySelector('.hero-text')
+const heroImg = document.querySelector('.hero-img')
 menu.onclick=()=>{
     menu.classList.toggle('bx-x')
     navList.classList.toggle('open')    
+}
+window.onload = ()=>{
+    setTimeout(() => {
+            heroText.classList.remove('showdown')
+    }, 500);
+    setTimeout(()=>{
+        heroImg.classList.remove('showdown')
+    },700)
 }

d8b338cdbc2a0a74009616c71a299c9ff50378da
HASSAN <151738906+hassanalaahassan@users.noreply.github.com>
2025-05-03 06:30:08 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,26 @@ const menu = document.querySelector('#menu-icon')
 const navList = document.querySelector('.nav-list')
 const heroText = document.querySelector('.hero-text')
 const heroImg = document.querySelector('.hero-img')
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach(entry=>{
+        if(entry.isIntersecting){
+            entry.target.classList.remove('translate-right')
+            entry.target.classList.remove('translate-left')
+            observer.unobserve(entry.target);
+        }
+    })
+},{
+    rootMargin:'-40px'
+})
+const aboutText = document.querySelectorAll('.about-text')
+const aboutImage = document.querySelectorAll('.about-img')
+
+aboutText.forEach((text)=>{
+    observer.observe(text)
+})
+aboutImage.forEach((image)=>{
+    observer.observe(image)
+})
 menu.onclick=()=>{
     menu.classList.toggle('bx-x')
     navList.classList.toggle('open')    
